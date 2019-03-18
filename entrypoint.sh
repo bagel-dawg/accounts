7b046cfbb564d7ae8de076c30e1ace182b42df08
Diamon Wiggins <diamon.wiggins@swimlane.com>
2019-03-18 19:26:37 -0400

    Removing flask db migration commands from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-while true; do
-    flask db migrate
-    if [[ "$?" == "0" ]]; then
-        flask db upgrade
-        break
-    fi
-    echo Upgrade command failed, retrying in 5 secs...
-    sleep 5
-done
 exec flask run --host=0.0.0.0
